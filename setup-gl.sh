4e60a25b7650d3f95e1785383289c66fd9d5e7f9
beetled-rt <97132737+beetled-rt@users.noreply.github.com>
2024-04-15 14:28:48 -0400

    Initial changes (#1)
    * GitLab incorporated and deployable
    
    * All 'gl-test' references to 'gitlab'.  Inject add'l key for 'ansible' user.

---
+++ setup-gl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#mkdir ~/.ansible/roles
+#ansible-galaxy collection install -r requirements.yaml
+#ansible-playbook kvm_provision.yaml
+
+#Uncomment to install additional systems as well
+#ansible-playbook kvm_provision.yaml --extra-vars "vm_name=ipa"
+
+# BeetleD added below
+ansible-playbook gl_provision.yaml -vv -l lab02
