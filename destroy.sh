a07f4def09c8cde307a932a9780949261c328a9f
beetled-rt <darryl.beetle@roguething.com>
2024-04-29 12:25:50 -0400

    IPA1 unable to resolve 'tang'.  Switched to ip address.  Plus shell cleanup.

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -i hosts/ivlab kvm_remove.yml -vv -u ${USER}
+ansible-playbook -i hosts/ivlab kvm_remove.yml -vv --ask-become-pass --ask-pass -u ${USER}
 #ansible-playbook -i hosts/nglaptop kvm_remove.yml -vv --ask-become-pass --ask-pass -u ${USER}
